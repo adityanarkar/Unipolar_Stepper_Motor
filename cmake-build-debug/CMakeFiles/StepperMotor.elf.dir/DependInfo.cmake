0286cc93221c4d46c43a99480f5e5c281718dd4b
Aditya Narkar <aditya.narkar25@gmail.com>
2019-05-14 16:35:09 -0500

    Unipolar stepper motor 28byj-48

---
+++ cmake-build-debug/CMakeFiles/StepperMotor.elf.dir/DependInfo.cmake
@@ -0,0 +1,80 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/startup/startup_stm32f407xx.s" "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/cmake-build-debug/CMakeFiles/StepperMotor.elf.dir/startup/startup_stm32f407xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "ARM_MATH_CM4"
+  "ARM_MATH_MATRIX_CHECK"
+  "ARM_MATH_ROUNDING"
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  "__FPU_PRESENT=1"
+  "__packed=__attribute__((__packed__))"
+  "__weak=__attribute__((weak))"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/cmake-build-debug/CMakeFiles/StepperMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/cmake-build-debug/CMakeFiles/StepperMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/cmake-build-debug/CMakeFiles/StepperMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/cmake-build-debug/CMakeFiles/StepperMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/cmake-build-debug/CMakeFiles/StepperMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/cmake-build-debug/CMakeFiles/StepperMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/cmake-build-debug/CMakeFiles/StepperMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/cmake-build-debug/CMakeFiles/StepperMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/cmake-build-debug/CMakeFiles/StepperMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/cmake-build-debug/CMakeFiles/StepperMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/cmake-build-debug/CMakeFiles/StepperMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/cmake-build-debug/CMakeFiles/StepperMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/cmake-build-debug/CMakeFiles/StepperMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/cmake-build-debug/CMakeFiles/StepperMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/Src/main.c" "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/cmake-build-debug/CMakeFiles/StepperMotor.elf.dir/Src/main.c.obj"
+  "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/Src/stm32f4xx_hal_msp.c" "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/cmake-build-debug/CMakeFiles/StepperMotor.elf.dir/Src/stm32f4xx_hal_msp.c.obj"
+  "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/Src/stm32f4xx_it.c" "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/cmake-build-debug/CMakeFiles/StepperMotor.elf.dir/Src/stm32f4xx_it.c.obj"
+  "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/Src/system_stm32f4xx.c" "/Users/adityanarkar/Aditya/Spring-2019/High-Assuarance-Systems/Projects/StepperMotor/cmake-build-debug/CMakeFiles/StepperMotor.elf.dir/Src/system_stm32f4xx.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "ARM_MATH_CM4"
+  "ARM_MATH_MATRIX_CHECK"
+  "ARM_MATH_ROUNDING"
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  "__FPU_PRESENT=1"
+  "__packed=__attribute__((__packed__))"
+  "__weak=__attribute__((weak))"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
